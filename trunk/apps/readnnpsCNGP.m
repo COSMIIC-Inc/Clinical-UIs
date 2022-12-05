8569613782d21967a8916e526ca4cf95ea06c3eb
rhart <rhart@03bc5d11-0c36-4e61-bad9-4efbbe88aa87>
2022-12-05 21:44:29 +0000

    variety of bug fixes and feature enhancements.  The main impact is better import and export between diagram creator, profiler, pattern editor as well as import from nnps files.
    git-svn-id: https://jdc-consulting.unfuddle.com/svn/jdc-consulting_matlabapi@257 03bc5d11-0c36-4e61-bad9-4efbbe88aa87

--- trunk/apps/readnnpsCNGP.m
+++ trunk/apps/readnnpsCNGP.m
@@ -1,7 +1,11 @@
-function [success, channels, nodes, groups] = readnnpsCNG()
+function [success, channels, nodes, groups, patterns] = readnnpsCNGP()
 % read the channels, nodes and groups from the nnps file
 % and arrange them in the manner that patternedit app wants
 % success=1 if read correctly.  0 if not
+% NOTE: Updated to sort the FG into the numerical order.  Next I will need
+% to correct for a skipped number, since IDs can be from 1-255
+% 
+
 % 	channels	6x4 cell   indexed by node,chan 
 %   first remove MES channels 
 % 		{'name' } char  muscle name, 
@@ -19,6 +23,15 @@ function [success, channels, nodes, groups] = readnnpsCNG()
 % 		{'name' } char 
 % 		{'type' } char PG, BP, PM or CT   - 
 % 		{'notes'} char set to ''
+%  patterns 130x1 cell array of structures, sorted by group, node, channel
+%       {'group'}
+%       {'node'}
+%       {'ch'}
+%       {'cmdID'}
+%       {'n'}
+%       {'cmd'}
+%       {'pw'}
+%       {'amp'}
 success=0;
 % % read GroupSets
 [fn, pn]=uigetfile('*.nnps','Get NNPS configuration File');
@@ -31,6 +44,11 @@ ngTags=size(gTags,2);
 %
 allGroupSet=xDoc.getElementsByTagName('GroupSet');
 numGS=allGroupSet.getLength;
+
+% I need to either set numGS to the maximum GS ID rather than length.
+% or because gsCell has the group ID included, I could wait until
+% the "groups" asignment to insert blanks as needed.
+
 gsCell=cell(numGS,ngTags); % create cell array to enter data
  % % get the data
 for k=0:allGroupSet.getLength-1  % for each groupset - zero based for DOM
@@ -38,11 +56,18 @@ for k=0:allGroupSet.getLength-1  % for each groupset - zero based for DOM
     for j=1:7 % 1 based indexing for matlab array
         thisList = thisListitem.getElementsByTagName(gTags(j));
         thisElement = thisList.item(0);
-        if ~isempty(thisElement.getFirstChild)
-            gsCell(k+1,j) = thisElement.getFirstChild.getData;
+        if ~isempty(thisElement)
+            if ~isempty(thisElement.getFirstChild)
+                gsCell(k+1,j) = thisElement.getFirstChild.getData;
+            end
         end
     end
 end
+% At this point I need to sort the gcCell cell array by groupsetID which is
+% column 1
+[~,Ix]=sort(cellfun(@str2num,gsCell(:,1))); gsCellSorted=gsCell(Ix,:);
+gsCell=gsCellSorted;
+
 % % translate moduleRecordID to node
 myTagsMod={'moduleType','moduleName','nodeNumber','moduleRecordID'};
 nmodTags=size(myTagsMod,2);
@@ -92,6 +117,93 @@ for i=1:numCH
         end
     end
 end
+
+% % read ChannelSets (patterns)
+
+myTagsChS={'channelName','ChannelData','eachChannel_Id','moduleRecordID','groupSet_Id'};
+nTags=size(myTagsChS,2);
+% note there are five tags I have chosen
+% [fn, pn]=uigetfile('*.nnps','Get NNPS configuration File');
+% xmlfile=fullfile(pn,fn);
+% xDoc=xmlread(xmlfile);
+allChan=xDoc.getElementsByTagName('ChannelSet');
+numCHS=allChan.getLength;
+chSCell=cell(numCHS,nTags); % create cell array to enter data
+% % get the data
+for k=0:allChan.getLength-1  % for each chan - zero based for DOM
+    thisListitem = allChan.item(k); %for each groupset, need to parse items
+    for j=1:nTags % 1 based indexing for matlab array
+        thisList = thisListitem.getElementsByTagName(myTagsChS(j));
+        thisElement = thisList.item(0);
+        if ~isempty(thisElement.getFirstChild)
+            chSCell(k+1,j) = thisElement.getFirstChild.getData;
+        end
+    end
+end
+% for each of the above elements I need to arrange into the format of
+%       {'group'}
+%       {'node'}
+%       {'ch'}
+%       {'cmdID'}
+%       {'n'}
+%       {'cmd'}
+%       {'pw'}
+%       {'amp'}
+% with the patterns structure sorted by group, node ch
+% base64str=chSCell{1,2}
+% chdata=base64decodeX(base64str)
+% n=size(chdata,2)/3
+% cmdpwamp=reshape(chdata,[3,n]; command, pw, amp, by row where
+% 
+% % intermediate
+nodeModArray=zeros(numMod,2); % want to use numbers rather than characters
+for i=1:numMod
+nodeModArray(i,1)=str2num(modCell{i,3}); %'nodeNumber'
+nodeModArray(i,2)=str2num(modCell{i,4}); %'moduleRecordID'
+end
+% use format nodeindx=find(nodeModArray(:,2)==27)
+% node=nodeModArray(nodeindex,1);
+% from there I should be able to find the indices and pick the node
+
+CidGSarray=zeros(numGS,2);
+for i=1:numGS
+CidGSarray(i,1)=str2num(gsCell{i,4}); % groupOutputCommandID
+CidGSarray(i,2)=str2num(gsCell{i,1}); % groupSet_Id
+end
+% use format gsindx=find(CidGSarray(:,2)==27)
+% cmdID=CidGSarray(gsindex,1);
+% from there I should be able to find the indices and pick the node
+pat=cell(numCHS,1); %empty cell
+% create a pattern cell array of structures that is numCHS long
+for pRow=1:numCHS % channel sets are another name for channnel patterns
+    gsid=str2num(chSCell{pRow,5}); %'groupSet_Id'
+    modRec=str2num(chSCell{pRow,4}); %moduleRecordID
+    ecid=str2num(chSCell{pRow,3}); %'eachChannel_Id'
+
+    pat{pRow}.group=str2num(chSCell{pRow,5});
+    nodeindex=find(nodeModArray(:,2)==modRec); %from moduleRecordID
+    pat{pRow}.node=nodeModArray(nodeindex,1);
+
+    pat{pRow}.ch=str2num(chSCell{pRow,1}); %channelName
+    gsindex=find(CidGSarray(:,2)==gsid); %'groupSet_Id'
+    pat{pRow}.cmdID=CidGSarray(gsindex,1);
+    base64str=chSCell{pRow,2};
+    chdata=double(base64decodeX(base64str));
+    n=size(chdata,2)/3;
+    pat{pRow}.n=n;
+    cmdpwamp=reshape(chdata,[3,n]); % command, pw, amp,
+    pat{pRow}.cmd=cmdpwamp(1,:);
+    pat{pRow}.pw= cmdpwamp(2,:);
+    pat{pRow}.amp= cmdpwamp(3,:);
+end
+% % next need to sort the pat array by group,node, chan
+%re-sort all patterns by group, node, ch
+M = [cellfun(@(x) x.group, pat), ...
+    cellfun(@(x) x.node, pat), ...
+    cellfun(@(x) x.ch, pat)];
+[~,i] = sortrows(M);
+patterns = pat(i);
+      
 
 % % Convert to correct format
 
@@ -176,13 +288,18 @@ for i=1:numCH
 end
 
 % groups
-nnps.groups=cell(numGS,1);
+% to correctly asign groups, I need to use max(groupSetID) to set the size
+% of the cell array, then use the current group ID to set index j
+fgMax=max(cellfun(@str2num,gsCell(:,1))); %maximum FG in the gsCell cell array
+nnps.groups=cell(fgMax,1); % one row for each possible FG
+% nnps.groups=cell(numGS,1);
 %fill in cell structures
 for i=1:numGS
-    nnps.groups{i}.name=gsCell{i,2};
-    nnps.groups{i}.cmdID=str2num(gsCell{i,4});
-    nnps.groups{i}.download=str2num(gsCell{i,7});
-    nnps.groups{i}.notes=gsCell{i,3};
+    j=str2num(gsCell{i,1}); % this is the row, where we insert into group
+    nnps.groups{j}.name=gsCell{i,2};
+    nnps.groups{j}.cmdID=str2num(gsCell{i,4});
+    nnps.groups{j}.download=str2num(gsCell{i,7});
+    nnps.groups{j}.notes=gsCell{i,3};
 end
 
 % % export 
@@ -191,3 +308,4 @@ channels = nnps.channels;
 nodes = nnps.nodes;
 groups = nnps.groups;
 success=1;
+
